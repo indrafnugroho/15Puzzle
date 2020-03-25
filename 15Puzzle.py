755eaa50b3f6a9fc3e42c4032db7b9773d490951
Indra Nugroho <indrafbrngrh@gmail.com>
2020-03-25 08:46:38 +0700

    seems like i hv to remake my program

--- 15Puzzle.py
+++ 15Puzzle.py
@@ -7,16 +7,47 @@ import heapq
 
 class Node:
 	def __init__(self, m, x, y, newX, newY, l, p):
-		self.parent = p
-		self.matrix = self.swapTiles(m,x,y,newX,newY)
-		self.cost = 1000000 #infinite val
-		self.misplacedTiles = 1000000
-		self.level = l
-		self.xBlank = newX
-		self.yBlank = newY
+		self.setParent(p)
+		self.setMatrix(m)
+		self.setCost(1000000) #infinite val
+		self.setMisplacedTiles(1000000)
+		self.setLevel(l)
+		self.setXBlank(newX)
+		self.setYBlank(newY)
+		self.swapTiles(x,y,newX,newY)
 
-	# def __lt__(self, other):
-	# 	return self.cost < other.cost
+	def setParent(self,p):
+		self._parent = p
+	def setMatrix(self,m):
+		self._matrix = m
+	def setCost(self,c):
+		self._cost = c
+	def setMisplacedTiles(self,mt):
+		self._misplacedTiles = mt
+	def setLevel(self,l):
+		self._level = l
+	def setXBlank(self,x):
+		self._xBlank = x
+	def setYBlank(self,y):
+		self._yBlank = y
+
+	def getParent(self):
+		return self._parent
+	def getMatrix(self):
+		return self._matrix
+	def getCost(self):
+		return self._cost
+	def getMisplacedTiles(self):
+		return self._misplacedTiles
+	def getLevel(self):
+		return self._level
+	def getXBlank(self):
+		return self._xBlank
+	def getYBlank(self):
+		return self._yBlank
+
+	def __lt__(self, other):
+		return self.getCost() < other.getCost()
 
 	def findBlankTile(self, m) :
 		x = -1
@@ -34,14 +65,12 @@ class Node:
 
 		return x,y
 
-	def swapTiles(self, m, xInit, yInit, xNew, yNew):
-		mTemp = m
-
+	def swapTiles(self, xInit, yInit, xNew, yNew):
+		mTemp = self.getMatrix()
 		temp = mTemp[xNew][yNew]
 		mTemp[xNew][yNew] = mTemp[xInit][yInit]
 		mTemp[xInit][yInit] = temp
-
-		return mTemp
+		self.setMatrix(mTemp)
 
 class PuzzleNode:
 	n = 4
@@ -96,8 +125,8 @@ class PuzzleNode:
 		if (n is None):
 			return
 		else:
-			self.printPath(n.parent)
-			self.printMatrix(n.matrix)
+			self.printPath(n.getParent())
+			self.printMatrix(n.getMatrix())
 			print()
 
 	def isMoveValid(self,x,y) :
@@ -107,26 +136,26 @@ class PuzzleNode:
 		#make PrioQueue
 		prioQ = []
 		root = Node(mInit, x, y, x, y, 0, None)
-		root.misplacedTiles = self.countMisplacedTiles(mInit,mGoal)
-		root.cost = self.calcCost(root.level, mInit, mGoal)
+		root.setMisplacedTiles(self.countMisplacedTiles(mInit,mGoal))
+		root.setCost(self.calcCost(root.getLevel(), mInit, mGoal))
 		heapq.heappush(prioQ, root)
 
 		while (len(prioQ) != 0):
-			min = heapq.heappop(prioQ)
-			if (min.misplacedTiles == 0):
-				self.printPath(min)
+			minPuzzle = heapq.heappop(prioQ)
+			if (minPuzzle.getMisplacedTiles() == 0):
+				self.printPath(minPuzzle)
 				break
 			else :
 				for i in range(self.n):
-					newXBlank = min.xBlank + self.row[i]
-					newYBlank = min.yBlank + self.col[i]
+					newXBlank = minPuzzle.getXBlank() + self.row[i]
+					newYBlank = minPuzzle.getYBlank() + self.col[i]
 
 					if (self.isMoveValid(newXBlank,newYBlank)) : 
                 	
                 		#create a child node and calculate its cost 
-						child = Node(min.matrix, min.xBlank, min.yBlank, newXBlank, newYBlank, min.level + 1, min) 
-						child.misplacedTiles = self.countMisplacedTiles(child.matrix,mGoal)
-						child.cost = self.calcCost(child.level, child.matrix,mGoal) 
+						child = Node(minPuzzle.getMatrix(), minPuzzle.getXBlank(), minPuzzle.getYBlank(), newXBlank, newYBlank, minPuzzle.getLevel + 1, minPuzzle) 
+						child.setMisplacedTiles(self.countMisplacedTiles(child.matrix,mGoal))
+						child.setCost(self.calcCost(child.level, child.matrix,mGoal))
   	
                 		#Add child to list of live nodes 
 						heapq.heappush(prioQ, child)
@@ -146,60 +175,3 @@ def main() :
     	puzzle.solvePuzzle(initial,final,xBlank,yBlank)
     else:
     	print("Raiso")
-
-def testing():
-	initial = [[1, 2, 3, 4], [5, 6, 16, 8], [9, 10, 7, 11], [13, 14, 15, 12]]
-	xBlank = 1
-	yBlank = 2
-
-	final = ([1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16])
-
-	puzzle = PuzzleNode()
-	# root = Node(initial, xBlank, yBlank, xBlank, yBlank, 0, None)
-	# root.misplacedTiles = puzzle.countMisplacedTiles(initial,final)
-	# root.cost = puzzle.calcCost(root.level, initial, final)
-	# print(root.cost)
-	# print(puzzle.countMisplacedTiles(initial,final))
-
-	prioQ = []
-	root = Node(initial, xBlank, yBlank, xBlank, yBlank, 0, None)
-	root.misplacedTiles = puzzle.countMisplacedTiles(initial,final)
-	root.cost = puzzle.calcCost(root.level, initial, final)
-	heapq.heappush(prioQ, (root.cost,root))
-
-	# min = heapq.heappop(prioQ)
-
-	# print(len(prioQ))
-
-	while (len(prioQ) != 0):
-		min = heapq.heappop(prioQ)
-		if (min[1].misplacedTiles == 0):
-			puzzle.printPath(min[1])
-			# print("disini1")
-			break
-		else :
-			# print("disini2")
-			for i in range(puzzle.n):
-				newXBlank = min[1].xBlank + puzzle.row[i]
-				# print(min.xBlank, newXBlank)
-				newYBlank = min[1].yBlank + puzzle.col[i]
-				# print(min.yBlank, newYBlank)
-
-				if (puzzle.isMoveValid(newXBlank,newYBlank)) : 
-					# print("disini3")
-               		#create a child node and calculate its cost 
-					child = Node(min[1].matrix, min[1].xBlank, min[1].yBlank, newXBlank, newYBlank, min[1].level + 1, min[1]) 
-					# print(child.xBlank)
-					# print(child.yBlank)
-					# print(child.matrix)
-					# print(child.level)
-					child.misplacedTiles = puzzle.countMisplacedTiles(child.matrix,final)
-					child.cost = puzzle.calcCost(child.level, child.matrix,final)
-					# print(child.misplacedTiles)
-					# print(child.cost)
-  					
- #               	#Add child to list of live nodes 
-					heapq.heappush(prioQ, (child.cost,child))
-					print(child.matrix)
-
-testing()
